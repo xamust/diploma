c057ec30c96dfa7a9c71ca2314b6717d8989ee15
xamust <xamust@gmail.com>
2022-09-16 21:11:01 +0300

    ALL without tests....

--- internal/app/collect/collect.go
+++ internal/app/collect/collect.go
@@ -2,8 +2,6 @@ package collect
 
 import (
 	"github.com/sirupsen/logrus"
-	"log"
-	"os"
 )
 
 type Collect struct {
@@ -12,36 +10,15 @@ type Collect struct {
 	ParsingDataFiles map[string]string
 }
 
-func (c *Collect) Destroy() error {
-	c.Logger.Info("Start destroying...")
-	if err := c.Start(); err != nil {
-		return err
-	}
-	for s, s2 := range c.ParsingDataFiles {
-		log.Print(s2)
-		if err := os.Remove(s2); err != nil {
-			return err
-		}
-		c.Logger.Infof("%s удален, находиился по пути %s", s, s2)
-
-	}
-	c.Logger.Info("Удаление *.data файлов выполнен успешно!")
-	return nil
-}
-
 func (c *Collect) Start() error {
 	c.Logger.Info("Start collecting...")
-
 	//search *.data files...
 	result, err := c.searchDataFiles()
 	if err != nil {
 		return err
 	}
 	c.Logger.Info("Поиск *.data файлов выполнен успешно!")
-
-	//c.Logger.Print(result)
 	c.ParsingDataFiles = result
-
 	return nil
 }
 
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -10,7 +10,6 @@ import (
 	"server/internal/app/systemsProject"
 	"server/testing/emulator"
 	"sync"
-	"time"
 )
 
 type AppServer struct {
@@ -46,13 +45,8 @@ func (s *AppServer) configureLogger() error {
 
 // configure emulator
 func (s *AppServer) configureEmulator() {
-	//starting emulator...
-	s.mu.Lock()
 	go emulator.EmulatorMain()
-	s.mu.Unlock()
-	time.Sleep(5 * time.Second)
 	s.logger.Info("Эмулятор запущен успешно!")
-
 }
 
 // config route...
@@ -71,13 +65,6 @@ func (s *AppServer) configureCollect() error {
 	return s.collect.Start()
 }
 
-// config delete old data files...
-func (s *AppServer) configureDeleteOld() error {
-	s.collect = &collect.Collect{Logger: s.logger, Config: s.config.Collect}
-	s.logger.Info("Коллектор *.data для удаления старых файлов инициализирован успешно!")
-	return s.collect.Destroy()
-}
-
 // config systems....
 func (s *AppServer) configureSystems() {
 	s.systems = &systemsProject.SystemsProject{ParsingDataFiles: s.collect.ParsingDataFiles, Config: s.config.Systems}
@@ -86,12 +73,6 @@ func (s *AppServer) configureSystems() {
 
 func (s *AppServer) Start() error {
 
-	//configure delete old data files...
-	//if err := s.configureDeleteOld(); err != nil {
-	//	return err
-	//}
-
-	//todo:эмулятор срабатывает раньше, чем положенно... м.б перезапись файлов?
 	//configure emulator...
 	s.configureEmulator()
 
@@ -100,7 +81,6 @@ func (s *AppServer) Start() error {
 		return err //if logrus configure result err
 	}
 
-	s.wg.Wait()
 	//configure collecting...
 	if err := s.configureCollect(); err != nil {
 		return err
@@ -111,7 +91,7 @@ func (s *AppServer) Start() error {
 
 	//configure router...
 	go s.configureRouter()
-	s.logger.Infof("ATTENTION %v", s.config.BindAddr)
+
 	//handlers init...
 	s.handl = Handlers{s.logger, s.mux, s.systems}
 	s.logger.Info(fmt.Sprintf("Starting server (bind on %v)...", s.config.BindAddr)) // set message Info level about succesfull starting server...
